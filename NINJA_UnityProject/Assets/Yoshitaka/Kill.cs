71af9cff79cc98fc50c75ac9d827320bbff217af
Yoshi696 <qiandaijicheng@gmail.com>
2020-09-14 11:43:43 +0900

    シーケンス、ゲームオーバーの実装？

---
+++ NINJA_UnityProject/Assets/Yoshitaka/Kill.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill : MonoBehaviour
+{
+    //private GameObject Camera2;
+    public GameObject sphe;
+    public GameObject camera;
+    public GameObject play2;
+
+    void OnCollisionEnter(Collision other)
+    {
+       // Vector3 cam3 = Camera2.transform.position;
+
+        if (other.gameObject.tag == "Player")
+        {
+            play2.GetComponent<PlayerMovement>().enabled = false;
+            camera.GetComponent<Camera>().enabled = false;
+            Destroy(sphe);
+        }
+    }
+}
---
+++ NINJA_UnityProject/Assets/Yoshitaka/StartScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cont_Jump")) //Aボタンを押したら開始
+        {
+            SceneManager.LoadScene("MainGame");//ゲーム開始
+        }
+    }
+}
