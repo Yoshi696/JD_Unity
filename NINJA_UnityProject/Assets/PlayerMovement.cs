061e912d016ed99daa66997c4e36be69e06cf9bc
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2020-09-14 10:51:09 +0900

    加速度的なやつ

--- NINJA_UnityProject/Assets/PlayerMovement.cs
+++ NINJA_UnityProject/Assets/PlayerMovement.cs
@@ -6,12 +6,23 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed = 0.5f;
+    public float acceleration = 0;
 
     private void FixedUpdate()
     {
+        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            acceleration += 0.002f;
+            if (acceleration > 0.5f) acceleration = 0.5f;
+        }
+        else
+        {
+            acceleration -= 0.005f;
+            if (acceleration < 0) acceleration = 0;
+        }
         var pos = transform.position;
         pos += transform.right * Input.GetAxis("Horizontal") * speed;
-        pos += transform.forward * Input.GetAxis("Vertical") * speed;
+        pos += transform.forward * Input.GetAxis("Vertical") * (speed + acceleration);
         transform.position = pos;
 
     }
